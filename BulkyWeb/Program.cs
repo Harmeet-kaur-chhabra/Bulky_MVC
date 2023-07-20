10fa8935d03a1c7408ea28d1112f24d5cb30a440
harmeet.chhabra@kellton.com <harmeet.chhabra@kellton.com>
2023-07-20 11:46:24 +0530

    Identity in .Net Core

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -8,6 +8,8 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection")));
 
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
